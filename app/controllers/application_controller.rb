e13d4f644514b9edce8e07b962424af6007fd44e
Jess Malvin Chin <jess@aelogica.com>
2017-01-17 13:30:18 +0800

    Use the goodreads api gem to access api methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  def goodreads_client
+    @client ||= instantiate_goodreads_client
+  end
+
+  private
+
+  def instantiate_goodreads_client
+    consumer = OAuth::Consumer.new(ENV["GR_APP_KEY"], ENV["GR_APP_SECRET"], site: "http://www.goodreads.com")
+    access_token = OAuth::AccessToken.from_hash(consumer, oauth_token: current_user.access_token, oauth_token_secret: current_user.token_secret)
+    Goodreads.new(oauth_token: access_token)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    puts "GOODREADS USER ID #{goodreads_client.user_id}"
   end
 end
