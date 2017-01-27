0f262b580f82869c3a8e6f4e6432bf109f2f7819
Jess Malvin Chin <jmalvinchin@gmail.com>
2017-01-28 07:12:54 +0800

    Get the jsx version of react rails to work

---
+++ app/assets/javascripts/components/_books.js.jsx
@@ -0,0 +1,8 @@
+var Books = React.createClass({
+  render() {
+    return (
+      <div> <h1>Hello, World!</h1> </div>
+    )
+  }
+});
+
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if current_user.sign_in_count == 1
-      GoodreadsProcessorWorker.perform_async(current_user.access_token, current_user.token_secret, current_user.id)
-    else
-    end
-    @books = current_user.books
+    # @books = current_user.books
+    # @books = Book.all
   end
 end
